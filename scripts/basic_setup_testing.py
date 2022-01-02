a46accd6ea4a280463a2de2c77c98b251e0cb9d5
isajediknight <luke@isajediknight.com>
2022-01-02 02:06:29 -0600

    Reading in basic Stadium info

--- scripts/basic_setup_testing.py
+++ scripts/basic_setup_testing.py
@@ -40,6 +40,7 @@ if((OS_TYPE == 'windows')):
     MODULES_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '\\modules\\'
     CLASSES_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '\\classes\\'
     TESTS_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '\\tests\\'
+    STATIC_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '\\inputs\\static\\'
 elif((OS_TYPE == 'linux') or (OS_TYPE == 'macintosh')):
     # Clear Screen Linux / Mac
     os.system('clear')
@@ -50,9 +51,10 @@ elif((OS_TYPE == 'linux') or (OS_TYPE == 'macintosh')):
     MODULES_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '/modules/'
     CLASSES_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '/classes/'
     TESTS_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '/tests/'
+    STATIC_DIR = OUTPUT_FILE_DIRECTORY[:directories[-1]] + '/inputs/static/'
 
 # OS Compatibility for importing Class Files
-if((OS_TYPE == 'linux')):
+if((OS_TYPE == 'linux') or (OS_TYPE == 'macintosh')):
     sys.path.insert(0,'../classes/')
     sys.path.insert(0, '../modules/')
 elif((OS_TYPE == 'windows')):
@@ -66,9 +68,27 @@ import random
 # < --- Begin Custom Classes Import --- >
 # Benchmark for tracking the time it takes for the programs tasks
 from benchmark import *
+# Stadium Class
+from stadium import *
 # < ---  End  Custom Classes Import --- >
 
+# Record Total Runtime of the program
 total_runtime = Benchmark()
-print("Hello World")
+
+# Read in all 30 MLB Stadiums
+stadium_input_runtime = Benchmark()
+readfile = open(STATIC_DIR+'stadiums.tab','r')
+stadiums = []
+counter = 0
+for line in readfile:
+    if(counter != 0):
+        my_line = line.split('\t')
+        my_line[-1] = my_line[-1].strip()
+        stadiums.append(Stadium(*my_line))
+    counter += 1
+readfile.close()
+stadium_input_runtime.stop()
+print(str(len(stadiums))+" Stadiums Read In: " + stadium_input_runtime.human_readable_string())
+
 total_runtime.stop()
-print("Total Runtime: "+total_runtime.human_readable_string())
+print("Total Runtime: " + total_runtime.human_readable_string())
